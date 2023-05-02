a60bbf84ab7bbe03808c5ae62c1321c018dc7204
Salvatore De Paola <salvatoredepaola8@gmail.com>
2023-05-02 17:18:25 +0200

    inizio confronto

--- js/main.js
+++ js/main.js
@@ -1,45 +1,56 @@
 
 
 
+let randomNumber = [];
+while (randomNumber.length < 5) {
+    let nuovonumero = getRandomNumber(1, 5);
+    if (randomNumber.includes(nuovonumero)) {
+    } else{
+        randomNumber.push(nuovonumero)
+    }
+}
+const testo = document.getElementById("randomNumber").innerText = randomNumber;
 
+console.log("Random Number: ",randomNumber)
 
+setTimeout(cancellaNumeri, 3 * 1000);
 
-const testo = document.getElementById("randomNumber").innerText = numeri(2,5);
-console.log(testo)
+setTimeout(function () {
+    
+    const numeriUtente = domanda();
 
-setTimeout(stop, 3 * 1000);
+    console.log(numeriUtente)
+    
+}, 4 * 1000);
 
-function stop() {
+function cancellaNumeri() {
     document.getElementById("randomNumber").innerText = "";
 }
 
-setTimeout(domanda, 3.1 * 1000);
-
 function domanda() {
-    let domanda = parseInt(prompt("Quali numeri erano apparsi?"));
-    console.log(domanda)
-    if (domanda == testo) {
-        console.log("bravo")
-    } else{
-        console.log("no")
+    const numeriUtente = [];
+
+    while(numeriUtente.length < 5) {
+
+        const numeroDaInserire = numeriUtente.length + 1;
+        const numero = parseInt( prompt(`Inserisci il ${numeroDaInserire}° numero` ) );
+
+        if( numeriUtente.includes(numero)==false && numero>0 ) {
+            numeriUtente.push(numero);
+        }
+
     }
+    console.log("Numeri Utente: ",numeriUtente)
+
+    return numeriUtente;
+
 }
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * ((max + 1) - min) + min)
 }
 
-function numeri(quantità, numeromassimo) {
-    let randomNumber = [];
-    while (randomNumber.length < quantità) {
-        let nuovonumero = getRandomNumber(1, numeromassimo);
-        if (randomNumber.includes(nuovonumero)) {
-        } else{
-            randomNumber.push(nuovonumero)
-        }
-    }
-    return randomNumber;
-}
+
 
 
 
